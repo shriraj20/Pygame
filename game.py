2b6d43cc3f0df1f556a17cdeebffcf48becc7578
Shriraj Dash <Shriraj.dash2016@gmail.com>
2020-12-18 10:45:37 -0800

    Update game.py

--- game.py
+++ game.py
@@ -1,16 +1,9 @@
 import pygame
 import sys
-from playsound import playsound
-
-# # Input an existing wav filename
-# wavFile = input("Enter a wav filename: ")
-# # Play the wav file
-# playsound(wavFile)
-#
-# # Input an existing mp3 filename
-mp3File = input("Marshmello x Imanbek (Ft. Usher) - Too Much (Official Music Video).mp3")
-# Play the mp3 file
-playsound(mp3File)
+# import os
+# import pyglet
+# music = pyglet.resource.media('Marshmello x Imanbek (Ft. Usher) - Too Much (Official Music Video).mp3')
+# music.play()
 
 # initialize it
 pygame.mixer.pre_init(44100)
@@ -25,7 +18,13 @@ display = pygame.display.set_mode((window_width, window_height))
 
 # forever loop
 while True:
-    print("hello")
+    # os.system("aplay Marshmello x Imanbek (Ft. Usher) - Too Much (Official Music Video).mp3")
+
+    pygame.mixer.init(44100)
+
+    pygame.mixer.music.load("Marshmello x Imanbek (Ft. Usher) - Too Much (Official Music Video).mp3")
+
+    pygame.mixer.music.play(-1)
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
