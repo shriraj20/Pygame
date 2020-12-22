ff3b004c730ff96422e53cfc46166a928c9c7166
Shriraj Dash <Shriraj.dash2016@gmail.com>
2020-12-21 22:37:17 -0800

    Update game.py

--- game.py
+++ game.py
@@ -1,14 +1,7 @@
 import pygame
 import sys
-from getkey import getkey, keys
-# import os
-# import pyglet
-# music = pyglet.resource.media('Marshmello x Imanbek (Ft. Usher) - Too Much (Official Music Video).mp3')
-# music.play()
-pygame.mixer.pre_init(44100)
 
 # initialize it
-pygame.mixer.pre_init(44100)
 pygame.init()
 
 # configurations
@@ -25,21 +18,9 @@ pygame.mixer.music.play(0)
 
 # forever loop
 while True:
-    key = getkey()
-    if key == keys.UP:
-        ...  # Handle the UP key
-    elif key == keys.DOWN:
-        ...  # Handle the DOWN key
-    elif key == 'a':
-        pygame.mixer.music.pause()  # Handle the `a` key
-    elif key == 'Y':
-        ...  # Handle `shift-y`
-    else:
-        # Handle other text characters
-        buffer += key
-        print(buffer)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if pygame.key.get_pressed()[K_q]:
+            print("hello")
